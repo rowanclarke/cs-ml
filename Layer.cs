ff43aeb619f9049576064250330d00e5bcfc03a1
Rowan Clarke <clarker280@gmail.com>
2020-11-11 20:58:17 +0000

    Initial commit

---
+++ Layer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace MachineLearning.Core
+{
+    public class Layer
+    {
+        public delegate void Activation();
+        private int[] shape;
+        private Activation activation;
+        public Layer(int[] shape, Activation activation)
+        {
+            this.shape = shape;
+            this.activation = activation;
+        }
+
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MachineLearning.Core
+{
+    public class Model
+    {
+        private List<Layer> layers = new List<Layer>();
+
+        public Model(List<Layer> layers)
+        {
+            this.layers = layers;
+        }
+
+        public Model() { }
+
+        public void Add(Layer layer)
+        {
+            layers.Add(layer);
+        }
+    }
+}
